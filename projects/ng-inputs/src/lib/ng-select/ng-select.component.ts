0350192855f6324b2086728fc3383e842c68e257
DouglasSerena <douglas222serena@rede.ulbra.br>
2021-03-03 22:17:47 -0300

    feat: add select

--- projects/ng-inputs/src/lib/ng-select/ng-select.component.ts
+++ projects/ng-inputs/src/lib/ng-select/ng-select.component.ts
@@ -1,15 +1,21 @@
 import {
   Component,
+  ElementRef,
   forwardRef,
   Input,
   OnInit,
+  Renderer2,
   SimpleChange,
+  ViewChild,
 } from '@angular/core';
-import { ControlContainer, NG_VALUE_ACCESSOR } from '@angular/forms';
 import {
-  IObject,
-  NgInputDefaultComponent,
-} from '../ng-input-default.component';
+  ControlContainer,
+  FormControl,
+  FormControlDirective,
+  NG_VALUE_ACCESSOR,
+  SelectControlValueAccessor,
+} from '@angular/forms';
+import { IInputDefaultComponent, IObject } from '../input-default.interface';
 
 @Component({
   selector: 'dss-select',
@@ -24,16 +30,31 @@ import {
   ],
 })
 export class NgSelectComponent
-  extends NgInputDefaultComponent
-  implements OnInit {
+  extends SelectControlValueAccessor
+  implements OnInit, IInputDefaultComponent {
   @Input() label: string = 'Sem label: ';
 
-  @Input() options?: { label: string; value: string }[] = [];
-  @Input() path?: { [key: string]: string };
-  @Input() labelDefault?: string = 'Selecione uma opção';
+  @Input() field: 'group' | 'floating' = 'floating';
 
-  constructor(controlContainer: ControlContainer) {
-    super(controlContainer);
+  @Input() options?: any | { label: string; value: string }[] = [];
+  @Input() path?: { [key: string]: string };
+  @Input() labelDefault: string = 'Selecione uma opção';
+
+  @Input() readonly: boolean;
+  @Input() required: boolean = false;
+
+  @Input() errors: IObject[] = [
+    { required: `Preencha o campo a cima.` },
+    { email: 'Email esta errado.' },
+  ];
+
+  constructor(
+    private controlContainer: ControlContainer,
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) {
+    super(renderer, elementRef);
+    this.readonly = false;
   }
 
   ngOnInit() {
@@ -42,30 +63,13 @@ export class NgSelectComponent
       this.required =
         this.errors.find((errors) => errors.type === 'required') != undefined;
 
-    this.verifyObject();
+    this.formatOptions();
   }
 
-  ngOnChanges(params: { objects: SimpleChange }) {
-    if (!!params.objects && !!params.objects.currentValue) this.verifyObject();
-  }
-
-  verifyObject() {
-    // if (this.options?.length === 0 && this.objects?.length > 0) {
-    //   const getMultiLabels = (labels: any, label: string[]) => {
-    //     const lastLabel = label.concat([]);
-    //     const rest = label.splice(1, label.length - 1);
-    //     return lastLabel.length === 1
-    //       ? labels[lastLabel[0]]
-    //       : getMultiLabels(labels[lastLabel[0]], rest);
-    //   };
-    //   this.options?.forEach((object) => {
-    //     this.options.push({
-    //       label: getMultiLabels(object, this.path?.label.split('.')),
-    //       value: getMultiLabels(object, this.path?.value.split('.')),
-    //     });
-    //   });
-    //   this.options.unshift({ label: this.labelDefault, value: '' });
-    // }
+  ngOnChanges(params: { options: SimpleChange }) {
+    if (!!params.options && !!params.options.currentValue) {
+      this.formatOptions();
+    }
   }
 
   getError(error: IObject, value: 'key' | 'value') {
@@ -74,5 +78,78 @@ export class NgSelectComponent
     return value === 'key'
       ? this.control.errors && this.control.errors[key]
       : error[key];
+  }
+
+  formatOptions() {
+    const option: any[] = [];
+    option.push({ label: this.labelDefault, value: '' });
+
+    const isValid =
+      this.options.length > 0 &&
+      (typeof this.options[0].label !== 'string' ||
+        typeof this.options[0].value !== 'string');
+
+    if (!!this.path && this.options.length > 0 && isValid) {
+      const getMultiLabels = (labels: any, label: string[]): string => {
+        const lastLabel = label.concat([]);
+        const rest = label.splice(1, label.length - 1);
+
+        return lastLabel.length === 1
+          ? labels[lastLabel[0]]
+          : getMultiLabels(labels[lastLabel[0]], rest);
+      };
+
+      const key = Object.keys(this.path)[0];
+      const value = this.path[key];
+
+      this.options?.forEach((opt: any) =>
+        option?.push({
+          label: getMultiLabels(opt, key.split('.')),
+          value: getMultiLabels(opt, value.split('.')),
+        })
+      );
+    }
+
+    this.options = option;
+  }
+
+  // #############################
+
+  // ATTRIBUTE AND METHODS CONTROL
+
+  // #############################
+
+  @ViewChild(FormControlDirective, { static: true })
+  formControlDirective: FormControlDirective;
+  @Input() formControl: FormControl;
+  @Input() formControlName: string;
+
+  get control() {
+    return (
+      this.formControl ||
+      this.controlContainer?.control?.get(this.formControlName)
+    );
+  }
+
+  writeValue(value: any): void {
+    this.renderer.setProperty(this.elementRef.nativeElement, 'checked', value);
+  }
+
+  onChange: (_: any) => {};
+  registerOnChange(fn: (_: any) => {}): void {
+    this.onChange = fn;
+  }
+
+  onTouched: () => {};
+  registerOnTouched(fn: () => {}): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.renderer.setProperty(
+      this.elementRef.nativeElement,
+      'disabled',
+      isDisabled
+    );
   }
 }
